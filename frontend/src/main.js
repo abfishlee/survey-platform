c297883482120544a1d8a20b001af7646026bb8c
abfishlee <abfishlee@gmail.com>
2026-01-01 16:30:11 +0900

    vue import 수정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,21 @@
 import { createApp } from 'vue'
-import './style.css' // 스타일 파일이 있다면 유지
+import './style.css' 
 
-// [변경 전] import App from './App.vue'
-// [변경 후] 새로 만든 SurveyWorkbench 컴포넌트를 불러옵니다.
+// 새로 만든 SurveyWorkbench 컴포넌트
 import SurveyWorkbench from './components/SurveyWorkbench.vue'
 
-// Ag-Grid 스타일 (전역 설정이 편할 경우 여기서 import)
+/* ----------------------------------------------------
+   [수정] 누락된 스타일 및 라이브러리 추가
+   이 부분이 없어서 아이콘 404 에러 및 레이아웃 깨짐 발생
+---------------------------------------------------- */
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'animate.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js' // 모달, 툴팁 등 동작 필수
+
+// Ag-Grid 스타일
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
-// [변경] App 대신 SurveyWorkbench를 마운트합니다.
+// 앱 마운트
 createApp(SurveyWorkbench).mount('#app')
